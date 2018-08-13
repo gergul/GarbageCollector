0ce87d4ccca1f9b7368ddb232f1b871ea346b903
Gergul <mail0668@gmail.com>
2018-08-13 14:19:19 +0800

    增加测试用例

--- gc_test/main.cpp
+++ gc_test/main.cpp
@@ -1,4 +1,5 @@
 #include "gc.h"
+#include <Windows.h>
 
 class Test : public GCObject {
 public:
@@ -30,16 +31,42 @@ public:
 	}
 };
 
+void t1()
+{
+	for (int i = 0; i < 1000; ++i)
+	{
+		new Test;
+		Sleep(100);
+	}
+}
+
+void t2()
+{
+	for (int i = 0; i < 1000; ++i)
+	{
+		new Test;
+		Sleep(100);
+	}
+}
+
+void t3()
+{
+	for (int i = 0; i < 500; ++i)
+	{
+		GC::instance.collect();
+		Sleep(200);
+	}
+}
 
 void main()
 {
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);    //设置调试堆函数跟踪分配的方式 
 	_CrtSetBreakAlloc(0);
 
+	//Normal test
 	Test* p = new Test;
 	for (int i=0; i<100; ++i)
 		GC::instance.pin(p);
-
 	Test2* p2 = new Test2;
 	GC::instance.pin(p2);
 	p2->mTest = new Test;
@@ -50,9 +77,24 @@ void main()
 	GC::instance.unpin(p2);
 	GC::instance.collect();
 
-	Test t;
+	{
+		//测试栈
+		Test t;
+		GC::instance.collect();
+
+		//测试Placement new
+		p = new(&t)Test;
+		GC::instance.collect();
+	}
+	
+	//多线程测试
+	std::thread thread1(t1);
+	std::thread thread2(t2);
+	std::thread thread3(t3);
+	thread1.join();
+	thread2.join();
+	thread3.join();
 	GC::instance.collect();
 
-	p = new(&t)Test;
-	GC::instance.collect();
+	return;
 }
